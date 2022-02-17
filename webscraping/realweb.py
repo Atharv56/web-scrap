69ee036d15fcb52b58e04bdc34890cfd9d14772e
Atharv Salian <atharv44@gmail.com>
2022-02-17 21:23:05 +0530

    all jobs in python

--- webscraping/realweb.py
+++ webscraping/realweb.py
@@ -1,11 +1,23 @@
+from cgitb import text
 from bs4 import BeautifulSoup
 import requests
 
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 
-jobs = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx').text
+job = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
 #print(jobs)
+for jobs in job:
+    published_date = jobs.find('span', class_ = 'sim-posted').span.text
+    company_name  = jobs.find('h3', class_ = "joblist-comp-name").text.replace(' ', '')
+    skills = jobs.find('span', class_ = 'srp-skills').text.replace(' ', '')
+    print(skills)
+    print(company_name)
+    
 
-company_name  = soup.find('h3', class_ = "joblist-comp-name").text
-print(company_name)
+    print(f'''
+    Company Name : {company_name}
+    Required Skills : {skills}
+    Date Published : {published_date}
+    ''')
+    print('')
