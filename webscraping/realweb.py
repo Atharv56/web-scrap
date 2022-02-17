6588386067bff4034975859057d214843047e38f
Atharv Salian <atharv44@gmail.com>
2022-02-17 14:04:32 +0530

    company name

--- webscraping/realweb.py
+++ webscraping/realweb.py
@@ -1,1 +1,11 @@
 from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+soup = BeautifulSoup(html_text, 'lxml')
+
+jobs = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx').text
+#print(jobs)
+
+company_name  = soup.find('h3', class_ = "joblist-comp-name").text
+print(company_name)
