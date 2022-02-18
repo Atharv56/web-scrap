dc5913a8994c59ec0921b5dfcc5b7ffde315ed8e
Atharv Salian <atharv44@gmail.com>
2022-02-18 22:36:45 +0530

    web scraping completed

--- webscraping/realweb.py
+++ webscraping/realweb.py
@@ -10,7 +10,7 @@ def find_jobs():
     print(f"Filtering out : {unfamiliar_skill}")
     job = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
     #print(jobs)
-    for jobs in job:
+    for index, jobs in enumerate(job):
         published_date = jobs.find('span', class_ = 'sim-posted').span.text
         if 'few' in published_date:
 
@@ -19,11 +19,13 @@ def find_jobs():
             more_info = jobs.header.h2.a['href']
 
             if unfamiliar_skill not in skills:
+                with open(f'/Users/atharvsalian/Desktop/Github/web-scrap/web-scrap/webscraping/posts/{index}.txt', 'w') as f:
 
-                print(f"Company Name : {company_name.strip()}")
-                print(f"Required Skills : {skills.strip()}")
-                print(f"More Info : {more_info}")
-                print('')
+                    f.write(f"Company Name : {company_name.strip()} \n")
+                    f.write(f"Required Skills : {skills.strip()} \n")
+                    f.write(f"More Info : {more_info} \n")
+                print(f'File Created : {index}')
+                    
 
 if __name__ == '__main__':
     while True:
